60722e5926fc2974d0fc3f24e9d740b9c3798c8f
unknown <chasemillers@outlook.com>
2023-03-01 20:21:11 -0800

    added ability to post

--- src/main/java/com/managmentsystem/fullstackapi/controller/ProductController.java
+++ src/main/java/com/managmentsystem/fullstackapi/controller/ProductController.java
@@ -1,10 +1,20 @@
 package com.managmentsystem.fullstackapi.controller;
 
-
+import com.managmentsystem.fullstackapi.model.Product;
+import com.managmentsystem.fullstackapi.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
 
+    @Autowired
+    private ProductRepository productRepository;
 
+    @PostMapping("/product")
+    Product newPorduct(@RequestBody Product newProduct){
+        return productRepository.save(newProduct);
+    }
 }
