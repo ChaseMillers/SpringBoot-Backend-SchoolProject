992525183e135a86275303b9e86fa9c127ccb39b
unknown <chasemillers@outlook.com>
2023-03-01 20:26:13 -0800

    added ability to get all products

--- src/main/java/com/managmentsystem/fullstackapi/controller/ProductController.java
+++ src/main/java/com/managmentsystem/fullstackapi/controller/ProductController.java
@@ -3,9 +3,12 @@ package com.managmentsystem.fullstackapi.controller;
 import com.managmentsystem.fullstackapi.model.Product;
 import com.managmentsystem.fullstackapi.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -16,5 +19,10 @@ public class ProductController {
     @PostMapping("/product")
     Product newPorduct(@RequestBody Product newProduct){
         return productRepository.save(newProduct);
+    }
+
+    @GetMapping("/products")
+    List<Product> getAllProducts(){
+        return productRepository.findAll();
     }
 }
