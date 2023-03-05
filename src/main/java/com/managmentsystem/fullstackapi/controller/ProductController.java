7d21be059a3d9a49f21dc35cd1b3e5553138813d
unknown <chasemillers@outlook.com>
2023-03-04 19:11:02 -0800

    can now get user by ID

--- src/main/java/com/managmentsystem/fullstackapi/controller/ProductController.java
+++ src/main/java/com/managmentsystem/fullstackapi/controller/ProductController.java
@@ -1,28 +1,33 @@
 package com.managmentsystem.fullstackapi.controller;
 
+import com.managmentsystem.fullstackapi.exeption.ProductNotFoundException;
 import com.managmentsystem.fullstackapi.model.Product;
 import com.managmentsystem.fullstackapi.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class ProductController {
 
     @Autowired
     private ProductRepository productRepository;
 
-    @PostMapping("/product")
+    @PostMapping("/api/postProduct")
     Product newPorduct(@RequestBody Product newProduct){
         return productRepository.save(newProduct);
     }
 
-    @GetMapping("/products")
+    @GetMapping("/api/getProducts")
     List<Product> getAllProducts(){
         return productRepository.findAll();
+    }
+
+    @GetMapping("/api/product/{id}")
+    Product getProductById(@PathVariable Long id){
+        return productRepository.findById(id)
+                .orElseThrow(()->new ProductNotFoundException(id));
     }
 }
